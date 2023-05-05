55d375a2ab6401dc8529bac2c47043dcd4f85af8
prasanjitha <graphicdis126@gmail.com>
2023-05-05 21:38:35 +0530

    Change button name

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -4,7 +4,7 @@ import { View } from 'react-native';
 function TestComponent(props) {
     return (
         <View>
-            <Button title="Click"></Button></View>
+            <Button title="Click Me"></Button></View>
     );
 }
 
