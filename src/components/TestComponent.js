da1f85deb6745703256b9fbb46873abe06394034
prasanjitha <graphicdis126@gmail.com>
2023-05-26 23:28:31 +0530

    add new button

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -7,6 +7,7 @@ function TestComponent(props) {
             <Button title="Click me"></Button>
             <Button title="Click"></Button>
             <Button title="Click me again"></Button>
+            <Button title="Touch"></Button>
         </View>
     );
 }
