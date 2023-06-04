d6d6bd288eff32a0d90712d9ff501e4644237008
prasanjitha <graphicdis126@gmail.com>
2023-06-04 23:37:32 +0530

    remove additional button

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -4,11 +4,6 @@ import { View } from 'react-native';
 function TestComponent(props) {
     return (
         <View>
-            <Button title="Click me"></Button>
-            <Button title="Click me"></Button>
-            <Button title="Click me"></Button>
-            <Button title="Click me"></Button>
-            <Button title="Click me"></Button>
             <Button title="Button click"></Button>
         </View>
     );
